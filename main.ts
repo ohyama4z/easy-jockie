fd57c9826d985881e6bbd726edca74339a7ccc05
ohyama4z <ohyama4z000@gmail.com>
2024-12-01 15:05:49 +0900

    m!playをつけて返す

--- main.ts
+++ main.ts
@@ -1,9 +1,26 @@
-import { Hono } from 'hono'
+import { createBot, Intents, startBot } from "./deps.ts"
 
-const app = new Hono()
-
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+const DISCORD_TOKEN = Deno.env.get("DISCORD_TOKEN") || ""
+const bot = createBot({
+  token: DISCORD_TOKEN,
+  intents: Intents.Guilds | Intents.GuildMessages | Intents.MessageContent,
 })
 
-Deno.serve(app.fetch)
+bot.events.messageCreate = (b, message) => {
+  if (message.isFromBot) return
+
+  if (message.content.startsWith("m!")) return
+
+  console.log(message)
+
+  if (
+    message.content.includes("youtube.com") ||
+    message.content.includes("youtu.be")
+  ) {
+    b.helpers.sendMessage(message.channelId, {
+      content: `m!play ${message.content}`,
+    })
+  }
+}
+
+await startBot(bot)
